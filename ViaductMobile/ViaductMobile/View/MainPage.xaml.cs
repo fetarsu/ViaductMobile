c1dfbdb58f8cec36747cd55e180062e38cb2a323
Rafał <s16849@pjwstk.edu.pl>
2021-01-23 21:18:45 +0100

    test ikonek 4

--- ViaductMobile/ViaductMobile/View/MainPage.xaml.cs
+++ ViaductMobile/ViaductMobile/View/MainPage.xaml.cs
@@ -48,15 +48,15 @@ namespace ViaductMobile
             string userPermission = loggedUser.Permission;
             if (userPermission.Equals("Admin"))
             {
-                ToolbarItem employeesPanel = new ToolbarItem() { Text = "Pracownicy", IconImageSource = "employees.png", Order = ToolbarItemOrder.Primary, Priority = 0 };
+                ToolbarItem employeesPanel = new ToolbarItem() { Text = "Pracownicy", IconImageSource = ImageSource.FromFile("employees.png"), Order = ToolbarItemOrder.Primary, Priority = 0 };
                 employeesPanel.Clicked += MoveToEmployeePanelClicked;
                 this.ToolbarItems.Add(employeesPanel);
             }
             if(userPermission.Equals("Admin") || loggedUser.DeliverRate > 0)
             {
-                ToolbarItem deliveryCart = new ToolbarItem() { Text = "Karta dostaw", IconImageSource = "delivery.png", Order = ToolbarItemOrder.Primary, Priority = 0};
-                ToolbarItem adressesPanel = new ToolbarItem() { Text = "Adresy", IconImageSource = "house.png", Order = ToolbarItemOrder.Primary, Priority = 1 };
-                ToolbarItem pizzasPanel = new ToolbarItem() { Text = "Produkty", IconImageSource = "pizza.png", Order = ToolbarItemOrder.Primary, Priority = 2 };
+                ToolbarItem deliveryCart = new ToolbarItem() { Text = "Karta dostaw", IconImageSource = ImageSource.FromFile("delivery.png"), Order = ToolbarItemOrder.Primary, Priority = 0};
+                ToolbarItem adressesPanel = new ToolbarItem() { Text = "Adresy", IconImageSource = ImageSource.FromFile("house.png"), Order = ToolbarItemOrder.Primary, Priority = 1 };
+                ToolbarItem pizzasPanel = new ToolbarItem() { Text = "Produkty", IconImageSource = ImageSource.FromFile("pizza.png"), Order = ToolbarItemOrder.Primary, Priority = 2 };
                 this.ToolbarItems.Add(pizzasPanel);
                 this.ToolbarItems.Add(adressesPanel);
                 this.ToolbarItems.Add(deliveryCart);
@@ -64,10 +64,10 @@ namespace ViaductMobile
                 adressesPanel.Clicked += MoveToAdressesPanelClicked;
                 pizzasPanel.Clicked += MoveToPizzasPanelClicked;
             }
-            ToolbarItem userPanel = new ToolbarItem() { Text = "Panel użytkownika", IconImageSource = "user.png", Order = ToolbarItemOrder.Primary, Priority = 0 };
+            ToolbarItem userPanel = new ToolbarItem() { Text = "Panel użytkownika", IconImageSource = ImageSource.FromFile("user.png"), Order = ToolbarItemOrder.Primary, Priority = 0 };
             userPanel.Clicked += MoveToUserPanelClicked;
             this.ToolbarItems.Add(userPanel);
-            ToolbarItem logout = new ToolbarItem() { Text = "Wyloguj", IconImageSource = "logout.png", Order = ToolbarItemOrder.Primary, Priority = 0 };
+            ToolbarItem logout = new ToolbarItem() { Text = "Wyloguj", IconImageSource = ImageSource.FromFile("logout.png"), Order = ToolbarItemOrder.Primary, Priority = 0 };
             logout.Clicked += MoveToLogout;
             this.ToolbarItems.Add(logout);
         }
